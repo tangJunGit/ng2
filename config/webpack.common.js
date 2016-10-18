831b3f11abf5788d87eb49a5e1e2b060ece0774a
tangjun <1031500697@qq.com>
2016-10-18 09:52:53 +0800

    update webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -6,6 +6,7 @@ var helpers = require('./helpers');
 module.exports = {
   entry: {
     'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
     'app': './src/main.ts'
   },
 
@@ -52,7 +53,7 @@ module.exports = {
 
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'polyfills'],
+      name: ['app', 'vendor', 'polyfills'],
       minChunks: Infinity
     }),
 
