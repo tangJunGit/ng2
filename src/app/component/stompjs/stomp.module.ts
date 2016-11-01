ddea110de8c5847bf42396bcc9f3cb6637a34c50
tangjun <1031500697@qq.com>
2016-11-01 10:31:27 +0800

    update stomp

--- src/app/component/stompjs/stomp.module.ts
+++ src/app/component/stompjs/stomp.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 
 import { stompRouting } from './stomp.routing';
-import { StompComponent }   from './stomp.component';
+import { StompComponent }   from './stomp-demo.component';
 import { StompService } from './stomp.service';
 import { ConfigService } from './config.service';
 
--- src/app/component/stompjs/stomp.routing.ts
+++ src/app/component/stompjs/stomp.routing.ts
@@ -1,7 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { StompComponent } from './stomp.component';
+import { StompComponent } from './stomp-demo.component';
 
 const routes: Routes = [
   { path: 'stomp', component: StompComponent },
