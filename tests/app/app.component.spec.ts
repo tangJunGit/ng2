e81d0018efec937c9f6380a3c6a036c96644a3d2
tangjun <1031500697@qq.com>
2016-11-14 12:36:20 +0800

    angular2 test

--- tests/app/app.component.spec.ts
+++ tests/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from '../src/app/app.component';
+import { AppComponent } from '../../src/app/app.component';
 
 describe('1st tests', () => {
   it('true is true', () => expect(true).toBe(true));
