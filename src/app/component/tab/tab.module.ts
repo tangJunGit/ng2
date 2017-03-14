4c25b996cbceb1f87dcb2b7d52948c3831eb06fa
tangjun <1031500697@qq.com>
2017-03-14 14:58:32 +0800

    标签页

--- src/app/component/tab/tab.module.ts
+++ src/app/component/tab/tab.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TabComponent, TabsetComponent }   from './tab.component';   // 组件导入
+import { TabComponent, TabsetComponent }   from './tab.component';  
 
 @NgModule({
     imports: [
